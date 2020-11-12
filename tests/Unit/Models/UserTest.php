e1486d431ea75c69eadfd1ccc3495c6a769a6072
h4unt <nguyenthanhh4u@gmail.com>
2020-11-12 10:23:43 +0700

    Test User Model

---
+++ tests/Unit/Models/UserTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use Tests\ModelTestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use App\Models\Follow;
+use App\Models\Borrow;
+use App\Models\Like;
+use App\Models\Review;
+
+class UserTest extends ModelTestCase
+{
+    public function test_model_configuration()
+    {
+        $this->runConfigurationAssertions(new User(), [
+            'name',
+            'username',
+            'email',
+            'password',
+            'fullname',
+            'birthday',
+            'phone',
+            'email', 
+            'gender',
+            'role',
+        ],[
+            'password', 'remember_token',
+        ]);
+    }
+    public function test_primaryKey()
+    {
+        $this->assertPrimaryKey('user_id', new User());
+    }
+
+    public function test_follows_relation()
+    {
+        $m = new User();
+        $r = $m->follows();
+        $this->assertHasManyRelation($r, $m, new Follow(), 'user_id');
+    }
+
+    public function test_borrows_relation()
+    {
+        $m = new User();
+        $r = $m->borrows();
+        $this->assertHasManyRelation($r, $m, new Borrow(), 'user_id');
+    }
+
+    public function test_liked_relation()
+    {
+        $m = new User();
+        $r = $m->liked();
+        $this->assertHasManyRelation($r, $m, new Like(), 'user_id');
+    }
+
+    public function test_reviews_relation()
+    {
+        $m = new User();
+        $r = $m->reviews();
+        $this->assertHasManyRelation($r, $m, new Review(), 'user_id');
+    }
+}
