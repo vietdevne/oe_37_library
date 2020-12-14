8f05737719c15b4f3a64cd35d475f0bd0070379e
h4unt <nguyenthanhh4u@gmail.com>
2020-12-14 09:32:51 +0700

    fix logic chart

--- app/Repositories/RepositoryInterface/UserRepositoryInterface.php
+++ app/Repositories/RepositoryInterface/UserRepositoryInterface.php
@@ -4,4 +4,5 @@ namespace App\Repositories\RepositoryInterface;
 interface UserRepositoryInterface extends BaseRepositoryInterface
 {
     public function getQuerySearch($fullname, $role);
+    public function userCountByMonth($month);
 }
